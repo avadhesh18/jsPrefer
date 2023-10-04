923b2294b283f03495d78b0132f636d4c3486c17
avadhesh18 <avadhesh18@users.noreply.github.com>
2023-10-04 17:28:17 +0530

    --

--- newissue.php
+++ newissue.php
@@ -4,7 +4,7 @@ $title = getenv('ISSUE_TITLE');
 $title = trim(str_replace('[addnew]','',$title));
 $username = getenv('AUTHOR_USERNAME');
 $pageContent =  getenv('ISSUE_BODY');
-file_put_contents('docs/test/is.txt',$pageContent);
+file_put_contents('docs/tests/is.txt',$pageContent);
 $template = file_get_contents('template.html');
 
 function createSlug($t,$m=5){$t=trim(preg_replace('/[^a-zA-Z0-9]+/','-',strtolower($t)),'-');$w=explode('-',$t);return implode('-',array_slice($w,0,$m));}
